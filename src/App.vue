91471eb076c417235c70288aef1b1a987c95ea44
Moin Alam <52396153+Moin010@users.noreply.github.com>
2021-08-12 14:22:20 +0600

    comiit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+
+<script>
+
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
+export default {
+
+  components:{
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo,
+  },
+
+
+  name: 'app',
+
+  data () {
+    
+    return {
+      products: [
+        {name: 'Banana Skin',price:20},
+        {name: 'Shyny Star',price:40},
+        {name: 'Mango Milk',price:60},
+        {name: 'COCO',price:80},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
